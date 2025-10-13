302cad32c4fc1b8d3cd49c6c633b27357c16a408
Rafael Melo <rafaelmelom.dev@gmail.com>
2025-10-13 13:30:45 -0300

    UPDATE: auto script

--- 03_User_Applications_Configurations/update.sh
+++ 03_User_Applications_Configurations/update.sh
@@ -2,4 +2,6 @@
 
 # updating ghostty and nvim configuration 
 cp -r ~/.config/{ghostty,nvim,lazyvim} .config/
+rm -rf .config/lazyvim/.git
+
 echo ".config updated ...";
--- 04_Dotfiles/update.sh
+++ 04_Dotfiles/update.sh
@@ -2,4 +2,6 @@
 
 # updating dotfiles
 cp -r ~/{.bashrc,.oh-my-zsh,.p10k.zsh,.pki,.zshrc} .
+rm -rf .oh-my-zsh/.git
+
 echo "Dotfiles updated ...";
