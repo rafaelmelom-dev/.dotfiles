8f7af8f8e2c59a80cf2b6f2b53b62b0f87c2ecb6
Rafael Melo <rafaelmelom.dev@gmail.com>
2025-11-22 10:21:27 -0300

    UPDATE

--- 03_User_Applications_Configurations/update.sh
+++ 03_User_Applications_Configurations/update.sh
@@ -2,6 +2,6 @@
 
 # updating ghostty and nvim configuration 
 cp -r ~/.config/{ghostty,nvim,lazyvim} .config/
-rm -rf .config/lazyvim/.git
+rm -rf .config/lazyvim/.git .config/lazyvim/.gitignore
 
 echo ".config updated ...";
