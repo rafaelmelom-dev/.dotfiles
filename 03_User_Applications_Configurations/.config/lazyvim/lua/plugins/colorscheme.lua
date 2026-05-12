b2a446dd4bb077922d3e8e78dada2519dcdd9c01
Rafael Melo <rafaelmelom.dev@gmail.com>
2026-05-12 00:41:44 -0300

    UPDATE

--- 03_User_Applications_Configurations/.config/lazyvim/lua/plugins/colorscheme.lua
+++ 03_User_Applications_Configurations/.config/lazyvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,54 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000,
-    config = function()
-      require("onedark").setup({
-        style = "warmer",
-        transparent = true,
-        term_colors = true,
 
-        toggle_style_key = "<C-t>",
-        toggle_style_list = { "warmer", "light" },
-      })
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "night",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
+    -- {
+    --   "projekt0n/github-nvim-theme",
+    --   name = "github-theme",
+    --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    --   priority = 1000, -- make sure to load this before all the other start plugins
+    --   config = function()
+    --     local function apply_theme()
+    --       if vim.o.background == "light" then
+    --         vim.cmd.colorscheme("github_light_default")
+    --       else
+    --         vim.cmd.colorscheme("github_dark_default")
+    --       end
+    --     end
+    --
+    --     apply_theme()
+    --
+    --     vim.api.nvim_create_autocmd("OptionSet", {
+    --       pattern = "background",
+    --       callback = apply_theme,
+    --     })
+    --   end,
+    -- },
+    {
+        "folke/tokyonight.nvim",
+        opts = {
+            style = "night",
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+        },
     },
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    opts = {
-      transparent_mode = true,
+
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+                transparent_mode = true,
+            })
+
+            vim.cmd.colorscheme("gruvbox")
+        end,
     },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "gruvbox",
+        },
     },
-  },
 }
--- 03_User_Applications_Configurations/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ 03_User_Applications_Configurations/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,34 +1,46 @@
-return {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    local function apply_theme()
-      if vim.o.background == 'light' then
-        vim.cmd.colorscheme 'github_light_default'
-      else
-        vim.cmd.colorscheme 'github_dark_default'
-      end
-    end
-
-    vim.api.nvim_create_autocmd('OptionSet', {
-      pattern = 'background',
-      callback = apply_theme,
-    })
-
-    apply_theme()
-  end,
-}
-
 -- return {
---     'ellisonleao/gruvbox.nvim',
---     priority = 1000,
+--     'projekt0n/github-nvim-theme',
+--     name = 'github-theme',
+--     lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
 --     config = function()
---         require('gruvbox').setup {
---             transparent_mode = true,
---         }
+--         require('github-theme').setup {
 --
---         vim.cmd.colorscheme 'gruvbox'
+--             options = {
+--                 styles = {
+--                     comments = 'italic',
+--                     keywords = 'bold',
+--                     types = 'italic,bold',
+--                 },
+--             },
+--         }
+--         local function apply_theme()
+--             if vim.o.background == 'light' then
+--                 vim.cmd.colorscheme 'github_light_default'
+--             else
+--                 vim.cmd.colorscheme 'github_dark_default'
+--             end
+--         end
+--
+--         vim.api.nvim_create_autocmd('OptionSet', {
+--             pattern = 'background',
+--             callback = apply_theme,
+--         })
+--
+--         apply_theme()
 --     end,
 -- }
+
+return {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+        require('gruvbox').setup {
+            transparent_mode = true,
+        }
+
+        vim.cmd.colorscheme 'gruvbox'
+    end,
+}
+
+-- return {}
